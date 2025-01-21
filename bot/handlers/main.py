0b03432a42d7a2da2e87277f2020806533d76d09
tanzila009 <tanzilakhusanovaa@gmail.com>
2025-01-21 18:28:07 +0500

    f

--- bot/handlers/main.py
+++ bot/handlers/main.py
@@ -15,12 +15,34 @@ async def command_start_handler(message: Message) -> None:
     )
     rkb.adjust(2, repeat=True)
     rkb = rkb.as_markup(resize_keyboard=True, one_time_keyboard=True)
-    await message.answer(f'Bittasini tanla', reply_markup=rkb)
+    await message.answer(f'Bittasini tanlang', reply_markup=rkb)
 
 @main.message(F.text=='Customer')
 async def customer_handler(message: Message) -> None:
     await message.answer(f"Sz Customer tanladiz!")
 
-@main.message(F.text=='Developer')
-async def customer_handler(message: Message) -> None:
-    await message.answer(f"Sz Developer tanladiz!")
+@main.message(F.text == 'Developer')
+async def developer_handler(message: Message) -> None:
+    await message.answer("Sz Developer tanladiz!")
+    rkb = ReplyKeyboardBuilder()
+    rkb.add(
+        KeyboardButton(text="Backend"),
+        KeyboardButton(text="Frontend")
+    )
+    await message.answer("Tanlang:", reply_markup=rkb.as_markup(resize_keyboard=True))
+
+@main.message(F.text=='Backend')
+async def backend_handler(message:Message) -> None:
+    rkb = ReplyKeyboardBuilder()
+    rkb.add(
+        KeyboardButton(text="Python"),
+        KeyboardButton(text="Java"),
+        KeyboardButton(text="C++"),
+        KeyboardButton(text="JavaScript")
+    )
+    rkb.adjust(3, repeat=True)
+    rkb = rkb.as_markup(resize_keyboard=True, one_time_keyboard=True)
+    await message.answer(f"Programmer languages:", reply_markup=rkb)
+
+
+
